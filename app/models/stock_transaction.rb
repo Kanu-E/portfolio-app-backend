690819070ded16725bfcd42139fd86f05d8005de
kanu-e <chukwuemekakanu@gmail.com>
2021-03-07 23:45:27 -0600

    set validations

--- app/models/stock_transaction.rb
+++ app/models/stock_transaction.rb
@@ -1,3 +1,5 @@
 class StockTransaction < ApplicationRecord
     belongs_to :portfolio
-end
+    validates :quantity, :transaction_type, presence: true
+    validates :transaction_type, inclusion: { in: %w(open close),
+ end
